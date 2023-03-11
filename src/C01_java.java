556c7adb8e81bff450d4cc344b0b5893e31e5c4d
Azat Özay <ozayazat0@gmail.com>
2023-03-11 18:24:18 +0300

    v3

--- src/C01_java.java
+++ src/C01_java.java
@@ -2,5 +2,6 @@ public class C01_java {
     public static void main(String[] args) {
         System.out.println("Hello gitHub!");
         System.out.println("Cmd cok kullanisli");
+        System.out.println("Bugunun sonunu gorebilecek miyiz");
     }
 }
